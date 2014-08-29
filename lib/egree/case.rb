f95c10a0f993ed0740e84f61e25c0312131dac93
Joel Junström <joel.junstrom@oktavilla.se>
2014-08-29 16:08:21 +0200

    Egree::Case
    A case has a name and an array of signature types.
    We can add parties and documents.

---
+++ lib/egree/case.rb
@@ -0,0 +1,43 @@
+module Egree
+  class Case
+    attr_reader :name, :signature_types
+
+    def initialize name, signature_types
+      @name = name
+      self.signature_types = signature_types
+    end
+
+    def signature_types= signature_types
+      types = Array(signature_types)
+
+      unknown_types = types - Egree.signature_types
+      if unknown_types.any?
+        raise unknown_signature_type_error(unknown_types)
+      end
+
+      @signature_types = types
+    end
+
+    def add_party party
+      self.parties << party
+    end
+
+    def parties
+      @parties ||= []
+    end
+
+    def add_document document
+      self.documents << document
+    end
+
+    def documents
+      @documents ||= []
+    end
+
+    private
+
+    def unknown_signature_type_error types
+      ArgumentError.new("Unknown signature types: #{types.join(", ")}. Valid types are: #{Egree.signature_types.join(", ")}")
+    end
+  end
+end
---
+++ spec/egree/case_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helper"
+require "egree/case"
+
+module Egree
+  def self.signature_types
+    [ "sms", "electronicId", "touch" ]
+  end
+
+  RSpec.describe Case do
+    it "requires a name" do
+      signature_case = Case.new "Agreement", Egree.signature_types
+
+      expect(signature_case.name).to eq "Agreement"
+    end
+
+    describe "signature types" do
+      it "requires at least one valid signature type" do
+        signature_case = Case.new "Agreement", Egree.signature_types.first
+
+        expect(signature_case.signature_types).to eq [ Egree.signature_types.first ]
+      end
+
+      it "throws an error if it is an unknown signature type" do
+        expect{ Case.new("Agreement", "unknown type") }.to raise_error(ArgumentError)
+      end
+    end
+
+    it "adds a party" do
+      signature_case = Case.new "Agreement", Egree.signature_types
+
+      party = double "Party"
+      signature_case.add_party party
+
+      expect(signature_case.parties).to eq [party]
+    end
+
+    it "adds a document" do
+      signature_case = Case.new "Agreement", Egree.signature_types
+
+      document = double "Document"
+      signature_case.add_document document
+
+      expect(signature_case.documents).to eq [document]
+    end
+  end
+end
