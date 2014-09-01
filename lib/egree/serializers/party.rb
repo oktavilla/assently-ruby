a2bac40fcebc48840e22102e500a29fbfc1113fb
Joel Junström <joel.junstrom@oktavilla.se>
2014-09-01 12:07:52 +0200

    Egree::Serializers::Party
    Implements to_api_hash and serialize

--- lib/egree/serializers/party.rb
+++ lib/egree/serializers/party.rb
@@ -1,7 +1,16 @@
 module Egree
   module Serializers
     module Party
+      def self.serialize party
+        JSON.pretty_generate to_api_hash(party)
+      end
+
       def self.to_api_hash party
+        {
+          "Name" => party.name,
+          "EmailAddress" => party.email,
+          "SocialSecurityNumber" => party.social_security_number
+        }
       end
     end
   end
