6eb2fdc79c95ca3320c2c71627ec2e57455338b5
Ivan Novosad <ivan.novosad@gmail.com>
2015-06-10 11:53:49 +0200

    Fix create_case specs

--- spec/egree/client/get_case_spec.rb
+++ spec/egree/client/get_case_spec.rb
@@ -9,21 +9,22 @@ require "egree/party"
 module Egree
   describe Egree::Client do
     let :client do
-      Egree::Client.new ENV["EGREE_USERNAME"], ENV["EGREE_PASSWORD"], :test
+      Egree::Client.new ENV["EGREE_API_KEY"], ENV["EGREE_API_SECRET"], :test
     end
 
-    describe "#get_case_url" do
+    describe "#get_case" do
       describe "with a reference_id as argument" do
-        it "sends the getviewcaseurl query with the case reference id json" do
+        it "sends the getcase query with the case reference id json" do
           allow(client).to receive :post
 
           reference_id = Egree::Case.generate_reference_id
+          serialized_json = "{\n  \"id\": \"#{reference_id}\"\n}"
 
-          expect(Egree::Serializers::ReferenceIdSerializer).to receive(:serialize).with(reference_id.to_s).and_return "reference-id-json"
+          expect(Egree::Serializers::ReferenceIdSerializer).to receive(:serialize).with(reference_id.to_s).and_return "reference_id"
 
-          client.get_case_url reference_id
+          client.get_case reference_id
 
-          expect(client).to have_received(:post).with "/apiv1/getviewcaseurlquery", "reference-id-json"
+          expect(client).to have_received(:post).with "/api/v2/getcase", serialized_json
         end
       end
 
@@ -33,16 +34,17 @@ module Egree
 
           reference_id = double "Egree::ReferenceId"
           signature_case = double "Egree::Case", reference_id: reference_id
+          serialized_json = "{\n  \"id\": \"#{reference_id}\"\n}"
 
-          expect(Egree::Serializers::ReferenceIdSerializer).to receive(:serialize).with(reference_id).and_return "reference-id-json"
+          expect(Egree::Serializers::ReferenceIdSerializer).to receive(:serialize).with(reference_id).and_return "reference-id"
 
-          client.get_case_url signature_case
+          client.get_case signature_case
 
-          expect(client).to have_received(:post).with "/apiv1/getviewcaseurlquery", "reference-id-json"
+          expect(client).to have_received(:post).with "/api/v2/getcase", serialized_json
         end
       end
 
-      describe "when the case exists", vcr: { cassette_name: "Egree_Client/_get_case_url/case_exists" } do
+      describe "when the case exists", vcr: { cassette_name: "Egree_Client/_get_case/case_exists" } do
         let :reference_id do
           Egree::Case.generate_reference_id
         end
@@ -53,13 +55,13 @@ module Egree
 
         describe "result" do
           it "is successfull" do
-            result = client.get_case_url reference_id
+            result = client.get_case reference_id
 
             expect(result.success?).to be true
           end
 
           it "has the url as the response" do
-            result = client.get_case_url reference_id
+            result = client.get_case reference_id
 
             expect(result.response).to match "https://test.assently.com"
           end
@@ -67,15 +69,15 @@ module Egree
       end
 
       describe "when there is no matching case" do
-        describe "result", vcr: { cassette_name: "Egree_Client/_get_case_url/case_missing" } do
+        describe "result", vcr: { cassette_name: "Egree_Client/_get_case/case_missing" } do
           it "is not successfull" do
-            result = client.get_case_url "missing-case"
+            result = client.get_case "missing-case"
 
             expect(result.success?).to be false
           end
 
           it "has the error" do
-            result = client.get_case_url "missing-case"
+            result = client.get_case "missing-case"
 
             expect(result.errors[0]).to eq "E123 Permission Denied: Read"
           end
@@ -86,7 +88,7 @@ module Egree
     private
 
     def create_case reference_id = nil
-      signature_case = Egree::Case.new "Agreement", ["touch"]
+      signature_case = Egree::Case.new "Agreement", ["touch"], case_id: reference_id
       signature_case.add_party Egree::Party.new_with_attributes({
         name: "First Last",
         email: "name@example.com",
